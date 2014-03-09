2cc93a1cf320062b5f991ef11f96128ed765c0b8
sudhakar <sudhakarsjce@gmail.com>
2014-03-09 14:59:50 -0700

    added boostrap_helper for heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  include BootstrapFlashHelper
   
   def not_found
     raise ActionController::RoutingError.new('Not Found')
